34d96f2827048ff54260e1c91a9e35ae9fd57b4c
ektasi8 <48591057+ektasi8@users.noreply.github.com>
2021-03-30 11:31:19 +0530

    Update BusinessLogicRepository.cs

--- ShopBridge/BAL/BusinessLogicRepository.cs
+++ ShopBridge/BAL/BusinessLogicRepository.cs
@@ -12,11 +12,13 @@ namespace ShopBridge.BAL
         {
             _irepository = _repository;
         }
+        //this return all products details
         public List<ShopBridge> getProducts()
         {
             List<ShopBridge> lstProducts = _irepository.ListProducts();
             return lstProducts;
         }
+         //this add new products details
         public void Add(ShopBridge product)
         {
             _irepository.AddProduct(product);
@@ -26,11 +28,12 @@ namespace ShopBridge.BAL
             ShopBridge product = _irepository.EditProducts(id);
             return product;
         }
-
+        //this delete products details
         public void Delete(int id)
         {
             _irepository.DeleteProducts(id);
         }
+        //this update products details
         public void Update(ShopBridge product)
         {
             _irepository.UpdateProducts(product);
